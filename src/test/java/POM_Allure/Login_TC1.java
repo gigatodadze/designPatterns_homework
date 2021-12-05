2b283adc6a51c740ea10a2e22633f9d61f37494f
gigatodadze <72299807+gigatodadze@users.noreply.github.com>
2021-12-05 13:09:52 +0400

    proeqti daiqoqa

--- src/test/java/POM_Allure/Login_TC1.java
+++ src/test/java/POM_Allure/Login_TC1.java
@@ -1,9 +1,22 @@
 package POM_Allure;
 
 import io.qameta.allure.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Listeners(Config.class)
 @Epic("Regression Tests")
@@ -11,10 +24,11 @@ import org.testng.annotations.Test;
 public class Login_TC1 extends BaseTest{
 @BeforeTest
 public void setup(){
-    BaseTest baseTest=new BaseTest();
-    baseTest.initialize();
-    driver=getDriver();
-    driver.get("https://www.demoqa.com/books");
+//    BaseTest baseTest=new BaseTest();
+//    baseTest.initialize();
+    System.setProperty("webdriver.chrome.driver", "chromedriver96");
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://demoqa.com/books");
 
 }
 
